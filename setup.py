251179f51dffe33af1a34d1f72d51a58740f5b20
Omar Reid <oreid52@googlemail.com>
2018-09-03 14:35:06 +0100

    Add Setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='BarcaMonkey',
+    version='0.1.0',
+    author='Omar Reid',
+    author_email='oreid52@googlemail.com',
+    packages=['barcamonkey'],
+    description='Collects Odds Data ',
+    install_requires=[
+        "slackclient",
+        "xmltodict",
+        "beautifulsoup4"
+    ],
+)
